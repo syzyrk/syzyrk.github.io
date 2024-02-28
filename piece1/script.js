51abb55f0fe9ed91d0eedcabe00a6a8aff1142c7
syzyrk <78079531+syzyrk@users.noreply.github.com>
2024-02-28 17:26:07 +0100

    Delete piece1 directory

--- piece1/script.js
+++
@@ -1,9 +0,0 @@
-function confirmPayment() {
-  // Wyświetlanie animacji ładowania przez 5 sekund
-  document.getElementById("loading-section").style.display = "block";
-  setTimeout(function() {
-    // Po zakończeniu animacji, wyświetlanie komunikatu o niedostępności płatności
-    document.getElementById("loading-section").style.display = "none";
-    document.getElementById("payment-unavailable").style.display = "block";
-  }, 5000);
-}
