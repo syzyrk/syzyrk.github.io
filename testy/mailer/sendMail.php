05936ca4d071ab2c456630af4958d39a211207e8
syzyrk <78079531+syzyrk@users.noreply.github.com>
2024-05-06 13:03:52 +0200

    Create sendMail.php

---
+++ testy/mailer/sendMail.php
@@ -0,0 +1,17 @@
+<?php
+$host = $_GET['host'];
+$sendUser = $_GET['sendUser'];
+$password = $_GET['password'];
+$fromMail = $_GET['fromMail'];
+$fromName = $_GET['fromName'];
+$subject = $_GET['subject'];
+$content = $_GET['content'];
+
+echo $host;
+echo $sendUser;
+echo $password
+echo $fromMail;
+echo $fromName;
+echo $subject;
+echo $content;
+?>
