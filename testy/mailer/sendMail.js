ff24fa4eaf7cee326d63772ece3f3f4eb00f98d7
syzyrk <78079531+syzyrk@users.noreply.github.com>
2024-05-06 13:11:54 +0200

    Update sendMail.js

--- testy/mailer/sendMail.js
+++ testy/mailer/sendMail.js
@@ -11,3 +11,15 @@ function sendMail(a,b,c,d,e,f,g){
     setTimeout(5000);
     window.location.href = url;
 }
+
+function send(){
+    var inputValue1 = form.input1.value;
+    var inputValue2 = form.input2.value;
+    var inputValue3 = form.input3.value;
+    var inputValue4 = form.input4.value;
+    var inputValue5 = form.input5.value;
+    var inputValue6 = form.input6.value;
+    var inputValue7 = form.input7.value;
+
+    sendMail(inputValue1, inputValue2, inputValue3, inputValue4, inputValue5, inputValue6, inputValue7);
+}
